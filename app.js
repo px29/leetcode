14854f9afe3b445f00785abc895c3c2d68bdb9a6
Rui Nan <rui@stronghold.co>
2018-12-11 10:35:11 -0800

    merged

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const WordSearch = require("./topics/array/WordSearch79.js");
 
-console.log(typeof Searcha2DMatrix74.searchMatrix);
-const result = Searcha2DMatrix74.searchMatrix(
-  [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]],
-  3
+const result = WordSearch.exist(
+  [
+    ['A','B','C','E'],
+    ['S','F','C','S'],
+    ['A','D','E','E']
+  ],
+  "ABCCED"
 );
 console.log(result);
