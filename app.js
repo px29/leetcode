5217d22b665243819b4fbceed731ba9163eb212c
Rui Nan <nanruihit@gmail.com>
2019-01-31 22:18:46 -0800

    save

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const LC = require("./topics/array/lc152.js");
+const LC = require("./topics/walmart/lc873.js");
 
-const result = LC.maxProduct(
-  [2,3,-2,4]
+console.log(LC);
+const result = LC(
+  [2,4,7,8,9,10,14,15,18,23,32,50]
 );
 console.log(result);
