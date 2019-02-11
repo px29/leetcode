f626467cb6781f3c8bd258ad5f3c06dd350d2b53
Rui Nan <rui@stronghold.co>
2019-02-11 15:48:21 -0800

    save

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const LC = require('./topics/amazon/oa/ShortestPath.js')
+const LC = require('./topics/wish/keyboard.js');
 
-const result = LC([
-    [1, 1, 1, 0, 1],
-    [1, 0, 1, 1, 0],
-    [1, 1, 1, 0, 1],
-    [0, 1, 0, 1, 1],
-    [1, 1, 1, 1, 9],
-])
-console.log(result)
+const result = LC('1234', [[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+console.log(result);
---
+++ topics/wish/Keyboard.js
@@ -0,0 +1,39 @@
+function distance(prev, current) {
+    if (prev && current) {
+        const disX = Math.abs(prev[0] - current[0]);
+        const disY = Math.abs(prev[1] - current[1]);
+        if (disX === 2 || disY === 2) {
+            return 2;
+        } else if (disX === 1 || disY === 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    return -1;
+}
+
+function keyboard(string, keypad) {
+    const map = new Map();
+    for (let i = 0; i < keypad.length; i++) {
+        for (let j = 0; j < keypad[i].length; j++) {
+            map.set(keypad[i][j], [i, j]);
+        }
+    }
+    let result = 0;
+    let pre = undefined;
+    for (const s of string) {
+        if (pre) {
+            const prevLocation = map.get(pre);
+            const currentLocation = map.get(s);
+            const dis = distance(prevLocation, currentLocation);
+            if (dis) {
+                result += dis;
+            }
+        }
+        pre = s;
+    }
+    return result;
+}
+
+module.exports = keyboard;
