76608e85199d118756c0bee0a8b0191f3245aeaf
Rui Nan <nanruihit@gmail.com>
2019-02-11 20:09:55 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const LC = require("./topics/walmart/lc98.js");
+
+function Node(left = null, right = null, value) {
+  this.left = left;
+  this.right = right;
+  this.value = value;
+}
+
+const root = new Node(2);
+root.left = new Node(1);
+root.right = new Node(3);
 
 const result = LC(
   [
@@ -7,5 +18,6 @@ const result = LC(
     [0, 1, 0, 1, 1],
     [1, 1, 1, 1, 9]
   ]
+  root
 );
 console.log(result);
