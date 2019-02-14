6adfedb9c2ecedf4d56c04f70b08becac0476882
Rui Nan <rui@stronghold.co>
2019-02-13 18:36:32 -0800

    save

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
-const keyboard = require('./topics/wish/keyboard.js');
-const palindrome = require('./topics/wish/palindrome.js');
-const subsequence = require('./topics/wish/subsequence.js');
+const f = require('./topics/zillow/MineSwiper');
 
-// keypad
-console.error('Keypad:');
-let array = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
-let result = keyboard('5555', array); // expect 0;
+const result = f.generateBoard(4, 2);
 console.log(result);
-result = keyboard('123456789', array); // expect 10
-console.log(result);
-
-array = [['4', '1', '6'], ['2', '5', '7'], ['9', '3', '8']];
-result = keyboard('5555', array); // 5555
-console.log(result);
-
-
-// palindrome
-console.error('Palindrome:');
-
-let string = 'aba';
-result = palindrome(string);
-console.log(result);
-
-// subsequence
-console.error('Subsequence:');
-result = subsequence('cba');
-console.log(result);
-result = subsequence('');
-console.log(result);
-result = subsequence(' a b ');
-console.log(result);
+for (let i = 0; i < 10; i++) {
+    const r = f.handleClick(
+        parseInt(Math.random() * 10),
+        parseInt(Math.random() * 10),
+        result
+    );
+    console.log(r);
+}
